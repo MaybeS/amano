a7313b715b52c63760d1baf84a32042c5e947e75
Jiun Bae <maytryark@gmail.com>
2020-04-17 03:12:07 +0900

    Update images

--- detector/Detector/LibDetector.h
+++ detector/Detector/LibDetector.h
@@ -20,10 +20,10 @@ struct LIB_DETECTION Box {
 class LIB_DETECTION Detector;
 
 extern "C" LIB_DETECTION Detector * _stdcall DetectorInit(const char* filename, int AGpu);
-extern "C" LIB_DETECTION long _stdcall DetectorRelease(Detector * AHanlde);
-extern "C" LIB_DETECTION int _stdcall DetectorClassify(Detector * AHandle, const cv::Mat & AImage);
-extern "C" LIB_DETECTION std::vector<Box> * _stdcall DetectorDetect(Detector * AHandle, cv::Mat & AImage, bool refine = true);
-extern "C" LIB_DETECTION std::vector<Box> * _stdcall DetectorDetectImage(Detector * AHanlde, const char* filename);
-extern "C" LIB_DETECTION std::vector<Box> * _stdcall DetectorDetectMat(Detector * AHandle, unsigned char* pArray, int nArraySize);
-extern "C" LIB_DETECTION long _stdcall DetectorSetParam(Detector * AHandle, int DSize_W = 0, int DSize_H = 0, float AConfidenceThreshold = .0f, float ARefineThreshold = .0f);
+extern "C" LIB_DETECTION long _stdcall DetectorRelease(Detector * detector);
+extern "C" LIB_DETECTION int _stdcall DetectorClassify(Detector * detector, const cv::Mat & AImage);
+extern "C" LIB_DETECTION std::vector<Box> * _stdcall DetectorDetect(Detector * detector, cv::Mat & AImage, bool refine = true);
+extern "C" LIB_DETECTION std::vector<Box> * _stdcall DetectorDetectImage(Detector * detector, const char* filename);
+extern "C" LIB_DETECTION std::vector<Box> * _stdcall DetectorDetectMat(Detector * detector, unsigned char* pArray, int nArraySize);
+extern "C" LIB_DETECTION long _stdcall DetectorSetParam(Detector * detector, int DSize_W = 0, int DSize_H = 0, float AConfidenceThreshold = .0f, float ARefineThreshold = .0f);
 extern "C" LIB_DETECTION long _stdcall DetectorGetObjectString(int AObj_idx, char* AObjName, int AObjSize);
